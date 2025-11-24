bbcee2a6d21620d8e50ac99584b724092d433bb7
Thales Machado <thales.machado@olivasdigital.com.br>
2025-11-24 14:20:20 -0300

    feat: adjust the author name and the description of the pages

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,11 +2,11 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Thales Potocas",
+	author: "Thales",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Potocas Insights",
+	title: "Thales Insights",
 	// Meta property used as the default description meta property
-	description: "Insights do Potocas",
+	description: "Insights do Thales",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "pt-BR",
 	// Meta property, found in src/components/BaseHead.astro L:42
